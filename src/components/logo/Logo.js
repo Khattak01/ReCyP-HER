ab773d91a2238674169ed38ae7113484cc27215e
Khattak01 <mskhattak.ncs@gmail.com>
2022-02-02 12:54:51 +0500

    dashboard screen added

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Logo = ({ to, className }) => {
     return (
         <Link to={to || '/'} className={[classes.logo__box, className].join(" ")}>
-            <h1>Click Dummy</h1>
+            <h1>ReCyP:HER</h1>
         </Link>
     )
 }
